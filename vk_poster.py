8bb125db5d14d396107b5ab16c8abc1b29a9a411
aderpil21-source <aderpil21@gmail.com>
2026-09-20 20:54:05 +0200

    Update vk_poster.py

--- vk_poster.py
+++ vk_poster.py
@@ -106,8 +106,12 @@ def upload_photo_to_vk(image_path):
     group_id = str(OWNER_ID).lstrip('-')
     
     url_server = requests.get(
-        "https://api.vk.com/method/photos.getWallUploadServer",
-        params={'group_id': group_id, 'access_token': TOKEN, 'v': API_VERSION}
+        "https://api.vk.com/method/photos.getOwnerWallUploadServer",
+        params={
+            'group_id': str(OWNER_ID).lstrip('-'),
+            'access_token': TOKEN,
+            'v': API_VERSION
+        }
     ).json()
     
     if 'response' not in url_server:
